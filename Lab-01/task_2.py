df8cb874497fb2c89137124204f05113f54a6b0a
TonitoMC <joseameridac@gmail.com>
2026-01-19 20:09:13 -0600

    feat: add task 2 (Lab-01)

---
+++ Lab-01/task_2.py
@@ -0,0 +1,68 @@
+import numpy as np
+import pandas as pd
+
+# 1. Dirty dataset generation
+
+# Initialize empty list of dictionaries to store data
+data_list = []
+
+# Add 100 entries
+for i in range(100):
+    # Initialize row with random values, all '0's initially for 'Compro_Producto'
+    row = {
+        "Edad": np.random.randint(16, 90),
+        "Salario": np.random.uniform(4000, 50000),
+        "Compro_Producto": 0,
+    }
+    # Add row to list
+    data_list.append(row)
+
+# Turn list into DF
+df = pd.DataFrame(data_list)
+
+# Set 10 random entries in 'Edad' to NaN
+df.loc[df.sample(10).index, "Edad"] = np.nan
+
+# Set 10 random samples in 'Compro_Producto' to '1'
+df.loc[df.sample(10).index, "Compro_Producto"] = 1
+
+# 2. Data Inputter
+
+# Calculate mean for 'Edad' column
+mean = df["Edad"].mean()
+
+# Manually iterate through all rows replacing nans, bajillion built-in functions for this but it's instructions
+# This is also slower as far as I know but learning purposes & stuff
+for i, row in df.iterrows():
+    if pd.isna(row["Edad"]):
+        df.at[i, "Edad"] = mean
+
+# Pregunta extra: ¿En que situación usar el promedio sería una mala idea y sería mejor utilizar la mediana?
+
+# El promedio es una mala idea si se tienen outliers extremos. Por ejemplo, para la lista [1,1,x,1000]
+# (a parte de probablemente ser un error de entrada) el promedio es de 334 mientras que viendo los datos
+# crudos la mejor idea podría ser un valor cercano a 1. También se puede tomar un "approach" más visual
+# y basarse en la longitud de las colas para identificar el sesgo. Otro ejemplo sería el salaio o ingresos
+# mensuales promedios en USA donde el top 0.1% tiene del 13-14% de las riquezas. Aquí el promedio y
+# mediana de los ingresos son $75000 y $55000 aproximadamente, definitivamente deberíamos utilizar la
+# mediana.
+
+# Unbalanced data management
+
+
+# Function to balance the dataset
+def undersample(df, target, min, maj):
+    minority = df[df[target] == min]
+    majority = df[df[target] == maj]
+
+    n_min = len(minority)
+
+    majority_sample = majority.sample(n=n_min)
+
+    balanced_df = pd.concat([minority, majority_sample])
+
+    return balanced_df
+
+
+# Balance the dataset
+balanced_df = undersample(df, "Compro_Producto", 1, 0)
